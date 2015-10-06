e9df7c3a505e8b86ff93af072e5cd283fb276971
Frank S. Thomas <frank@timepit.eu>
2015-10-06 19:13:36 +0200

    we can't cast but need to call Refined.unsafeApply

--- shared/src/test/scala/eu/timepit/refined/MonocleTest.scala
+++ shared/src/test/scala/eu/timepit/refined/MonocleTest.scala
@@ -10,7 +10,7 @@ object MonocleTest extends App {
 
   type Minute = Int Refined Interval[W.`0`.T, W.`59`.T]
 
-  val minute = Lens[LocalDateTime, Minute](_.getMinute.asInstanceOf)(m => d => d.withMinute(m.get))
+  val minute = Lens[LocalDateTime, Minute](d => Refined.unsafeApply(d.getMinute))(m => d => d.withMinute(m.get))
 
   minute.set(10)  // ok
   //minute.set(61) // does not compile
